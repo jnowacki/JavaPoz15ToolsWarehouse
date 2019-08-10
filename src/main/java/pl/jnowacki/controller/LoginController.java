762bb328ab45ef8523538ec825095a3fb94fc5c6
Jedrzej Nowacki <contact@jnowacki.pl>
2019-08-10 14:21:17 +0200

    Login redirections

--- src/main/java/pl/jnowacki/controller/LoginController.java
+++ src/main/java/pl/jnowacki/controller/LoginController.java
@@ -25,6 +25,13 @@ public class LoginController extends HttpServlet {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
 
-        System.out.println(userService.checkIfValid(username, password));
+        if (userService.checkIfValid(username, password)) {
+            req.getSession().setAttribute("user", username);
+
+            resp.sendRedirect(getServletContext().getContextPath() + "/");
+        } else {
+            getServletContext().getRequestDispatcher("/login.jsp").forward(req, resp);
+        }
+
     }
 }
