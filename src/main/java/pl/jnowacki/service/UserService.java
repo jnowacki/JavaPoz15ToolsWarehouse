27f0679d1b42dc88d57dece777663747fcd3db7d
Jedrzej Nowacki <contact@jnowacki.pl>
2019-08-10 14:00:41 +0200

    User service interface

--- src/main/java/pl/jnowacki/service/UserService.java
+++ src/main/java/pl/jnowacki/service/UserService.java
@@ -1,16 +1,5 @@
 package pl.jnowacki.service;
 
-import pl.jnowacki.dao.UserDao;
-import pl.jnowacki.dao.UserDaoImpl;
-import pl.jnowacki.model.User;
-
-public class UserService {
-
-    private UserDao userDao = new UserDaoImpl();
-
-    boolean checkIfValid (String login, String password) {
-        User user = userDao.getUser(login);
-
-        return user.getLogin().equals(login) && user.getPassword().equals(password);
-    }
+public interface UserService {
+    boolean checkIfValid (String login, String password);
 }
---
+++ src/main/java/pl/jnowacki/service/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package pl.jnowacki.service;
+
+import pl.jnowacki.dao.UserDao;
+import pl.jnowacki.dao.UserDaoImpl;
+import pl.jnowacki.model.User;
+
+public class UserServiceImpl {
+
+    private UserDao userDao = new UserDaoImpl();
+
+    boolean checkIfValid (String login, String password) {
+        User user = userDao.getUser(login);
+
+        return user.getLogin().equals(login) && user.getPassword().equals(password);
+    }
+}
